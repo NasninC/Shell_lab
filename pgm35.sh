6cc67727e79a6351e4cd940df2d24eb3ba4c8276
Nasnin c <nasninc4@gmail.com>
2026-03-03 13:51:51 +0530

    print pyramid

--- pgm35.sh
+++ pgm35.sh
@@ -1,13 +1,20 @@
-read -p "Enter the number of rows:" n
-for ((i=1;i<=n;i++))
+#!/bin/bash
+
+read -p "Enter the number of rows: " n
+
+for ((i=1; i<=n; i++))
 do
-for ((j=i;j<n;j++))
-do
-echo -n ""
-done
-for ((k=1;k<=i;k++))
-do
-echo -n "$k"
-done
-echo ""
+    # Print spaces
+    for ((j=i; j<n; j++))
+    do
+        echo -n " "
+    done
+
+    # Print numbers from 1 to i
+    for ((k=1; k<=i; k++))
+    do
+        echo -n "$k "
+    done
+
+    echo
 done
