d15005f99abfe19cf0f9634352176fe88291b45a
Nasnin c <nasninc4@gmail.com>
2026-03-03 13:51:17 +0530

    print pyramid pattern
    Refactor script to print a pyramid pattern of stars.

--- pgm36.sh
+++ pgm36.sh
@@ -1,13 +1,20 @@
-read -p "Enter number of rows:" n
-for ((i=1;i<=n;i++))
+#!/bin/bash
+
+read -p "Enter the number of rows: " n
+
+for ((i=1; i<=n; i++))
 do
-for((j=i;j<n;j++))
-do
-echo -n ""
-done
-for ((k=1;k<=i;k++))
-do
-echo -n "*"
-done
-echo ""
+    # Print spaces
+    for ((j=i; j<n; j++))
+    do
+        echo -n " "
+    done
+
+    # Print stars
+    for ((k=1; k<=((2*i-1)); k++))
+    do
+        echo -n "*"
+    done
+
+    echo
 done
